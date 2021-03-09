6d2abc11b5c27c34a975c45a8027c3d1fdcad04b
Amman Ahmed <b6039889@my.shu.ac.uk>
2021-03-09 15:57:20 +0000

    updated info page

--- app/src/main/java/com/example/groupworksheffxplore/NorfolkParkPage.java
+++ app/src/main/java/com/example/groupworksheffxplore/NorfolkParkPage.java
@@ -2,13 +2,24 @@ package com.example.groupworksheffxplore;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NorfolkParkPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_norfolk_park_info);
+
     }
 
 }
